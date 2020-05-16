62b8ea3ae14d12c78376fe780a16c33c3381d0a1
BlankGodd <blankgodd33@gmail.com>
2020-05-17 00:26:06 +0100

    tictactoe game

--- tictactoe.py
+++ tictactoe.py
@@ -4,20 +4,19 @@
 import sys
 
 class Tictactoe:
-	dic = {1:[0,0],2:[0,1],3:[0,2],4:[1,0],5:[1,1],6:[1,2],7:[2,0],8:[2,1],9:[2,2]}
 
 	def __init__(self):
-		# board
+
 		self.bd = [['-','-','-'],['-','-','-'],['-','-','-']]
 
 		print('Welcome to Tictactoe')
-		
+			
 		print("Help to use board")
-		print(' 1 | 2 | 3')
+		print(' 7 | 8 | 9')
 		print('-----------')
 		print(' 4 | 5 | 6')
 		print('-----------')
-		print(' 7 | 8 | 9')
+		print(' 1 | 2 | 3')
 
 		print("Enter 0 to play against a machine or 1 for two users")
 		try:
@@ -30,55 +29,108 @@ class Tictactoe:
 		else:
 			self.play_machine()
 
-	def check_win(self, v):
-		if self.bd[0][0] == v and self.bd[0][1] == v and self.bd[0][2] == v:
+
+	def print_board(self):
+		bd = self.bd
+
+		print(' '+bd[0][0]+' | '+bd[0][1]+' | '+bd[0][2])
+		print('-----------')
+		print(' '+bd[1][0]+' | '+bd[1][1]+' | '+bd[1][2])
+		print('-----------')
+		print(' '+bd[2][0]+' | '+bd[2][1]+' | '+bd[2][2])
+
+
+	def check_win(self,v):
+		bd = self.bd
+
+		if bd[0][0] is v and bd[0][1] is v and bd[0][2] is v:
 			return True
-		elif self.bd[1][0] == v and self.bd[1][1] == v and self.bd[1][2] == v:
+		elif bd[1][0] is v and bd[1][1] is v and bd[1][2] is v:
 			return True	
-		elif self.bd[2][0] == v and self.bd[2][1] == v and self.bd[2][2] == v:
+		elif bd[2][0] is v and bd[2][1] is v and bd[2][2] is v:
 			return True
-		elif self.bd[0][0] == v and self.bd[1][0] == v and self.bd[2][0] == v:
+		elif bd[0][0] is v and bd[1][0] is v and bd[2][0] is v:
 			return True
-		elif self.bd[0][1] == v and self.bd[1][1] == v and self.bd[2][1] == v:
+		elif bd[0][1] is v and bd[1][1] is v and bd[2][1] is v:
 			return True
-		elif self.bd[0][2] == v and self.bd[1][2] == v and self.bd[2][2] == v:
+		elif bd[0][2] is v and bd[1][2] is v and bd[2][2] is v:
 			return True
-		elif self.bd[0][0] == v and self.bd[1][1] == v and self.bd[2][2] == v:
+		elif bd[0][0] is v and bd[1][1] is v and bd[2][2] is v:
 			return True
-		elif self.bd[0][2] == v and self.bd[1][1] == v and self.bd[2][0] == v:
+		elif bd[0][2] is v and bd[1][1] is v and bd[2][0] is v:
 			return True
 		return False
 
+
 	def set_board(self, player, v):
+		bd = self.bd
 		pos = 0
 		if player == 'p1':
 			pos = int(input("Player1: "))
 		else:
 			pos = int(input("Player2: "))
 
-		if pos == 1:
-			self.bd[0][0] == v
-		elif pos == 2:
-			self.bd[0][1] == v
-		elif pos == 3:
-			self.bd[0][2] == v
-		elif pos == 4:
-			self.bd[1][0] == v
-		elif pos == 5:
-			self.bd[1][1] == v
-		elif pos == 6:
-			self.bd[1][2] == v
-		elif pos == 7:
-			self.bd[2][0] == v
+		if pos == 7:
+			if bd[0][0] != '-':
+				print("Invalid location!")
+				self.set_board(player, v)
+			else:
+				bd[0][0] = v
 		elif pos == 8:
-			self.bd[2][1] == v
+			if bd[0][1] != '-':
+				print("Invalid location!")
+				self.set_board(player, v)
+			else:
+				bd[0][1] = v
 		elif pos == 9:
-			self.bd[2][2] == v
+			if bd[0][2] != '-':
+				print("Invalid location!")
+				self.set_board(player, v)
+			else:
+				bd[0][2] = v
+		elif pos == 4:
+			if bd[1][0] != '-':
+				print("Invalid location!")
+				self.set_board(player, v)
+			else:
+				bd[1][0] = v
+		elif pos == 5:
+			if bd[1][1] != '-':
+				print("Invalid location!")
+				self.set_board(player, v)
+			else:
+				bd[1][1] = v
+		elif pos == 6:
+			if bd[1][2] != '-':
+				print("Invalid location!")
+				self.set_board(player, v)
+			else:
+				bd[1][2] = v
+		elif pos == 1:
+			if bd[2][0] != '-':
+				print("Invalid location!")
+				self.set_board(player, v)
+			else:
+				bd[2][0] = v
+		elif pos == 2:
+			if bd[2][1] != '-':
+				print("Invalid location!")
+				self.set_board(player, v)
+			else:
+				bd[2][1] = v
+		elif pos == 3:
+			if bd[2][2] != '-':
+				print("Invalid location!")
+				self.set_board(player, v)
+			else:
+				bd[2][2] = v
 		else:
 			print("Invalid position")
 			self.set_board(player, v)
 
+
 	def play(self):
+		bd = self.bd
 		print("Player 1 to chose X or 0")
 		p1 = input(": ").upper()
 		if p1 == 'X':
@@ -91,21 +143,21 @@ class Tictactoe:
 		self.print_board()
 		win_p1 = False
 		win_p2 = False
-		if win_p1 or win_p2:
-			win = True
-		else:
-			win = False
 		count = 1
-		while not win:
+		while True:
 			print("Enter position to insert tag(X/O)")
 			if count%2 == 0:
 				self.set_board('p2', p2)
 				self.print_board()
 				win_p2 = self.check_win(p2)
+				if win_p2:
+					break
 			else:
 				self.set_board('p1', p1)
 				self.print_board()
 				win_p1 = self.check_win(p1)
+				if win_p1:
+					break
 			count += 1
 			if count == 10:
 				break
@@ -121,78 +173,9 @@ class Tictactoe:
 		else:
 			sys.exit(1)
 
-	def play_machine(self):
-		pass
-		
-	def print_board(self):
-
-		print(' '+self.bd[0][0]+' | '+self.bd[0][1]+' | '+self.bd[0][2])
-		print('-----------')
-		print(' '+self.bd[1][0]+' | '+self.bd[1][1]+' | '+self.bd[1][2])
-		print('-----------')
-		print(' '+self.bd[2][0]+' | '+self.bd[2][1]+' | '+self.bd[2][2])
-
-############################################################333
-
-bd = [['-','-','-'],['-','-','-'],['-','-','-']]
-
-def start():
-
-	print('Welcome to Tictactoe')
-		
-	print("Help to use board")
-	print(' 1 | 2 | 3')
-	print('-----------')
-	print(' 4 | 5 | 6')
-	print('-----------')
-	print(' 7 | 8 | 9')
-
-	print("Enter 0 to play against a machine or 1 for two users")
-	try:
-		c = int(input(": "))
-	except:
-		print("Invalid input")
-		sys.exit(1.5)
-	if c == 1:
-		play()
-	else:
-		play_machine()
 
 
-def print_board(bd):
-
-	print(' '+bd[0][0]+' | '+bd[0][1]+' | '+bd[0][2])
-	print('-----------')
-	print(' '+bd[1][0]+' | '+bd[1][1]+' | '+bd[1][2])
-	print('-----------')
-	print(' '+bd[2][0]+' | '+bd[2][1]+' | '+bd[2][2])
+if __name__ == "__main__":
+	Tictactoe()
 
 
-def check_win(bd,v):
-	if bd[0][0] is v and bd[0][1] is v and bd[0][2] is v:
-		return True
-	elif bd[1][0] is v and bd[1][1] is v and bd[1][2] is v:
-		return True	
-	elif bd[2][0] is v and bd[2][1] is v and bd[2][2] is v:
-		return True
-	elif bd[0][0] is v and bd[1][0] is v and bd[2][0] is v:
-		return True
-	elif bd[0][1] is v and bd[1][1] is v and bd[2][1] is v:
-		return True
-	elif bd[0][2] is v and bd[1][2] is v and bd[2][2] is v:
-		return True
-	elif bd[0][0] is v and bd[1][1] is v and bd[2][2] is v:
-		return True
-	elif bd[0][2] is v and bd[1][1] is v and bd[2][0] is v:
-		return True
-	return False
-
-
-
-
-
-print_board(bd)
-if check_win(bd, 'O'):
-	print(1)
-else:
-	print(0)
