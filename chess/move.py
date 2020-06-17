7db83f1dc19fec1323cb6934572e95830d6b6001
BlankGodd <blankgodd33@gmail.com>
2020-06-17 21:01:38 +0100

    interface design

---
+++ chess/move.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# Author:   @BlankGodd
+# moving pieces
+
+from board import Board
+
+class Piece:
+
+	def __init__(self):
+		pass
+
+	def move_pawn(self,a,b):
+		# check if pawn in position a,b-1
+		# move to pos a,b
+
