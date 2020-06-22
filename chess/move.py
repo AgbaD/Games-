b11e5a638f8bc8ed63b68c5354f3c72efa679e3a
BlankGodd <blankgodd33@gmail.com>
2020-06-22 11:30:03 +0100

    [chess] moving king

--- chess/move.py
+++ chess/move.py
@@ -5,6 +5,7 @@
 from pieces.pawn import Pawn
 from pieces.rook import Rook
 from pieces.bishop import Bishop
+from pieces.king import King
 
 
 class Move:
@@ -27,6 +28,13 @@ class Move:
 
 	def move_bishop(self,p,y,player):
 		piece = Bishop(self.board)
+		move = piece.move(p,y,player)
+		if not move:
+			print('Invalid move')
+		return move
+
+	def move_king(self,p,y,player):
+		piece = King(self.board)
 		move = piece.move(p,y,player)
 		if not move:
 			print('Invalid move')
